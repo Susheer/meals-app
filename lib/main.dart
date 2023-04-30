eb1451f646d7337d32f3aa13013b4c91ed2dcefb
sudheer <sudheergupta0508@gmail.com>
2023-04-30 22:34:26 +0530

    theams basic setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,30 @@ import 'package:meals_app/CategoriesScreen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final ThemeData theamData=ThemeData(
+    primarySwatch: Colors.pink,
+    accentColor: Colors.amber,
+    canvasColor: Color.fromRGBO(255, 255, 299, 1),
+    fontFamily: 'Raleway',
+    textTheme: ThemeData.light().textTheme.copyWith(
+        bodyText1: TextStyle(
+          color: Color.fromRGBO(20, 51, 51, 1),
+        ),
+        bodyText2: TextStyle(
+          color: Color.fromRGBO(20, 51, 51, 1),
+        ),
+        headline6: TextStyle(
+          fontSize: 20,
+          fontFamily: 'RobotoCondensed',
+          fontWeight: FontWeight.bold,
+        )),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: theamData,
       home: CategoriesScreen(),
     );
   }
