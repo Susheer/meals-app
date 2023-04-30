fb6bfa8ad0fac07ae405931ecf9e399b7a2cbef0
sudheer <sudheergupta0508@gmail.com>
2023-04-30 19:26:13 +0530

    dummy category added

--- lib/models/Category.dart
+++ lib/models/Category.dart
@@ -1,8 +1,8 @@
 import "package:flutter/material.dart";
 
-class Catefory {
+class Category {
   final String id;
   final String title;
   final Color color;
-  const Catefory({@required this.id, @required this.title, this.color=Colors.orange});
+  const Category({@required this.id, @required this.title, this.color=Colors.orange});
 }
